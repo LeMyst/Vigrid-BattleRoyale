4720e4569c72b15d77e4f694ce959a530ea301ff
KeganH <keganhollern@gmail.com>
2020-08-20 16:01:16 -0400

    New marker solution for DayZ Expansion Update

---
+++ Scripts/5_Mission/GUI/Map/BattleRoyaleMapMarkerZone.c
@@ -0,0 +1,127 @@
+//--- circle/oval marker 
+class BattleRoyaleMapMarkerZone extends ExpansionMapWidgetBase
+{
+    protected float f_SizeA;
+    protected float f_SizeB;
+    protected float f_Thickness;
+    protected int i_Color;
+
+    protected Widget m_Frame;
+    protected CanvasWidget m_Canvas;
+
+    void BattleRoyaleMapMarkerZone( Widget parent, MapWidget mapWidget, bool autoInit = true )
+	{
+        
+    }
+    void ~BattleRoyaleMapMarkerZone()
+    {
+
+    }
+
+    protected override void OnInit( Widget layoutRoot )
+    {
+        Class.CastTo( m_Frame, layoutRoot.FindAnyWidget( "marker_frame" ) );
+        Class.CastTo( m_Canvas, layoutRoot.FindAnyWidget( "marker_canvas" ) );
+
+
+        m_Frame.SetColor( ARGB( 0, 0, 0, 0 ) );
+        SetColor(ARGB(255,255,255,255));
+        SetSize_A(150);
+        SetSize_B(150);
+        SetThickness(2);
+        vector pos = "1000 10 1000";
+        SetPosition(pos);
+    }
+    override void Update( float pDt )
+	{
+        super.Update(pDt);
+
+        m_Canvas.Clear();
+        vector m_edgePos_A = GetPosition();
+        m_edgePos_A[0] = m_edgePos_A[0] + f_SizeA;
+
+        vector m_edgePos_B = GetPosition();
+        m_edgePos_B[2] = m_edgePos_B[2] + f_SizeB;
+
+        vector mapPos_edge_A = GetMapWidget().MapToScreen(m_edgePos_A);				
+        vector mapPos_edge_B = GetMapWidget().MapToScreen(m_edgePos_B);
+        vector mapPos_center = GetMapWidget().MapToScreen(GetPosition());
+
+        float distance_A = vector.Distance(mapPos_center,mapPos_edge_A);
+        float distance_B = vector.Distance(mapPos_center,mapPos_edge_B);
+
+        float canvas_width;
+        float canvas_height;
+        m_Canvas.GetSize(canvas_width,canvas_height);
+        float center_x = canvas_width / 2.0;
+        float center_y = canvas_height / 2.0;
+
+        if(((center_x + distance_A) > canvas_width) || ((center_y + distance_B) > canvas_height))
+        {
+            //Resize to fit
+            float new_x = (center_x + distance_A) + 2;
+            float new_y = (center_y + distance_B) + 2;
+            float new_size = Math.Max(Math.Max(new_x,new_y), 34.0);
+
+            m_Canvas.SetSize(new_size,new_size);
+        }
+
+        float cx = center_x;
+        float cy = center_y;
+        float a = distance_A;
+        float b = distance_B;
+
+        for(int i = 0; i < 360;i++)
+        {
+            float x1 = cx + (a * Math.Cos(i*Math.DEG2RAD));
+            float y1 = cy + (b * Math.Sin(i*Math.DEG2RAD));
+
+            float x2 = cx + (a * Math.Cos((i+1)*Math.DEG2RAD));
+            float y2 = cy + (b * Math.Sin((i+1)*Math.DEG2RAD));
+
+            m_Canvas.DrawLine(x1, y1, x2, y2, f_Thickness, i_Color);
+        }
+    }
+
+
+
+    override string GetLayoutPath()
+    {
+        return "BattleRoyale/GUI/layouts/map/royale_map_marker.layout";// custom BR widget structure
+    }
+    int GetColor()
+    {
+        return i_Color;
+    }
+    void SetColor(int color)
+    {
+        i_Color = color;
+    }
+    float GetSize_A()
+    {
+        return f_SizeA;
+    }
+    void SetSize_A(float value)
+    {
+        f_SizeA = value;
+    }
+    float GetSize_B()
+    {
+        return f_SizeB;
+    }
+    void SetSize_B(float value)
+    {
+        f_SizeB = value;
+    }
+    float GetThickness()
+    {
+        return f_Thickness;
+    }
+    void SetThickness(float value)
+    {
+        f_Thickness = value;
+    }
+    //GetPosition() -> returns world position
+    //SetPosition(vector position) --> update position in world coordinates
+
+}
--- Scripts/5_Mission/GUI/Map/ExpansionMapMenu.c
+++ Scripts/5_Mission/GUI/Map/ExpansionMapMenu.c
@@ -1,5 +1,10 @@
 modded class ExpansionMapMenu {
     protected ref BattleRoyaleClient m_BattleRoyaleClient;
+    protected ref BattleRoyaleMapMarkerZone m_CurrentZone;
+    protected ref BattleRoyaleMapMarkerZone m_NextZone;
+
+    
+
     override Widget Init()
     {
         BattleRoyaleBase DayZBR = GetBR();
@@ -8,9 +13,58 @@ modded class ExpansionMapMenu {
             Error("Failed to cast DayZBR to BattleRoyaleClient!");
         } 
 
-        return super.Init();
+        super.Init();
 
-        //TODO: add custom marker on open?
+        UpdateZones();
+
+        return layoutRoot;
     }
-    
+
+    void UpdateZones()
+    {
+        float radius;
+        vector center;
+
+        BattleRoyalePlayArea current_playarea = m_BattleRoyaleClient.GetPlayArea();
+        if(current_playarea)
+        {
+            if(!m_CurrentZone)
+            {
+                m_CurrentZone = new BattleRoyaleMapMarkerZone( layoutRoot, m_MapWidget );
+                m_Markers.Insert( m_CurrentZone );
+            }
+            m_CurrentZone.SetColor(ARGB(255, 255, 255, 255));
+            center = current_playarea.GetCenter();
+            radius = current_playarea.GetRadius();
+            m_CurrentZone.SetPosition(center);
+            m_CurrentZone.SetSize_A(radius);
+            m_CurrentZone.SetSize_B(radius);
+            m_CurrentZone.SetThickness(2);
+        }
+
+        BattleRoyalePlayArea next_playarea = m_BattleRoyaleClient.GetPlayArea();
+        if(next_playarea)
+        {
+            if(!m_NextZone)
+            {
+                m_NextZone = new BattleRoyaleMapMarkerZone( layoutRoot, m_MapWidget );
+                m_Markers.Insert( m_NextZone );
+            }
+            m_NextZone.SetColor(ARGB(255, 0, 0, 255));
+            center = next_playarea.GetCenter();
+            radius = next_playarea.GetRadius();
+            m_NextZone.SetPosition(center);
+            m_NextZone.SetSize_A(radius);
+            m_NextZone.SetSize_B(radius);
+            m_NextZone.SetThickness(2);
+        }
+    }
+
+    //ensure BR markers are rendering correct
+    override void Update( float timeslice )
+	{
+        UpdateZones(); //TODO: this shouldn't exist, instead we should call UpdateZones from BattleRoyaleClient (when we recieve our payload data with new zone info)
+
+        super.Update( timeslice );
+    }
 }
