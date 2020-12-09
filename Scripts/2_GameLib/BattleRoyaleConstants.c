39559e03f36cd7ba6ce0b603f5c6f0e5b7ecd6e8
KeganH <keganhollern@gmail.com>
2020-12-09 17:10:19 -0500

    Incremented version

--- Scripts/2_GameLib/BattleRoyaleConstants.c
+++ Scripts/2_GameLib/BattleRoyaleConstants.c
@@ -5,7 +5,7 @@
  * TODO: move these constants into something a bit easier for modders to work with, that way any 3rd party can launch their own BR off my framework
  */
 
-static const string BATTLEROYALE_VERSION = "0.1.q_namalsk";
+static const string BATTLEROYALE_VERSION = "0.1.r_namalsk";
 
 //--- default web endpoint
 static const string BATTLEROYALE_WEB_CLIENT_ENDPOINT = "https://dayzbr.dev";
