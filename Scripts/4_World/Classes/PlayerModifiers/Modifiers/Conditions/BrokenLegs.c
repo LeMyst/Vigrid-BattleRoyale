04ee79d193814df3b112f848e98a1da7cad6adf9
Myst <1592048+LeMyst@users.noreply.github.com>
2023-03-11 15:17:59 +0100

    Disable BrokenLegs

---
+++ Scripts/4_World/Classes/PlayerModifiers/Modifiers/Conditions/BrokenLegs.c
@@ -0,0 +1,17 @@
+modded class BrokenLegsMdfr
+{
+    override void OnActivate(PlayerBase player)
+	{
+        return;
+    }
+
+	override void OnReconnect(PlayerBase player)
+	{
+        return;
+	}
+
+    override void Activate()
+	{
+		return;
+	}
+}
