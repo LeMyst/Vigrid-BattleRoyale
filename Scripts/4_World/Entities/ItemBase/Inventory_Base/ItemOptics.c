02e90d1bb0f54144fcfd11c3f93b569e4bd54934
Myst <1592048+LeMyst@users.noreply.github.com>
2023-03-11 09:25:51 +0100

    Spawn Battery9V in optics

---
+++ Scripts/4_World/Entities/ItemBase/Inventory_Base/ItemOptics.c
@@ -0,0 +1,95 @@
+// Kobra Sights
+modded class KobraOptic {
+    override void EEInit()
+    {
+        super.EEInit();
+        if(HasEnergyManager())
+        {
+            GetInventory().CreateInInventory( "Battery9V" );
+        }
+    }
+}
+
+//
+modded class PSO1Optic {
+    override void EEInit()
+    {
+        super.EEInit();
+        if(HasEnergyManager())
+        {
+            GetInventory().CreateInInventory( "Battery9V" );
+        }
+    }
+}
+
+//
+modded class PSO11Optic {
+    override void EEInit()
+    {
+        super.EEInit();
+        if(HasEnergyManager())
+        {
+            GetInventory().CreateInInventory( "Battery9V" );
+        }
+    }
+}
+
+//
+modded class KazuarOptic {
+    override void EEInit()
+    {
+        super.EEInit();
+        if(HasEnergyManager())
+        {
+            GetInventory().CreateInInventory( "Battery9V" );
+        }
+    }
+}
+
+// Baraka Sights
+modded class ReflexOptic {
+    override void EEInit()
+    {
+        super.EEInit();
+        if(HasEnergyManager())
+        {
+            GetInventory().CreateInInventory( "Battery9V" );
+        }
+    }
+}
+
+// Combat Sights
+modded class M68Optic {
+    override void EEInit()
+    {
+        super.EEInit();
+        if(HasEnergyManager())
+        {
+            GetInventory().CreateInInventory( "Battery9V" );
+        }
+    }
+}
+
+// RVN Sights
+/*modded class  {
+    override void EEInit()
+    {
+        super.EEInit();
+        if(HasEnergyManager())
+        {
+            GetInventory().CreateInInventory( "Battery9V" );
+        }
+    }
+}*/
+
+// Mini Sights
+modded class PistolOptic {
+    override void EEInit()
+    {
+        super.EEInit();
+        if(HasEnergyManager())
+        {
+            GetInventory().CreateInInventory( "Battery9V" );
+        }
+    }
+}
