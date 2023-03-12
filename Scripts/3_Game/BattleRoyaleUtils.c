93e2f448999fd0b799260fa4e73699b70dc25ca8
Myst <1592048+LeMyst@users.noreply.github.com>
2023-03-12 12:16:06 +0100

    Update BattleRoyaleUtils.c

--- Scripts/3_Game/BattleRoyaleUtils.c
+++ Scripts/3_Game/BattleRoyaleUtils.c
@@ -34,4 +34,38 @@ class BattleRoyaleUtils extends Managed {
     static bool CheckLogLevel(int level) {
         return BATTLEROYALE_LOG_LEVEL >= level;
     }
+
+    static string GetDateTime(string dateTimeFormat = "%1.%2.%3 %4:%5:%6")
+    {
+        int year = 0;
+        int month = 0;
+        int day = 0;
+        int hour = 0;
+        int minute = 0;
+        int second = 0;
+
+        GetYearMonthDay(year, month, day);
+        GetHourMinuteSecond(hour, minute, second);
+        return string.Format(dateTimeFormat, day.ToStringLen(2), month.ToStringLen(2), year.ToStringLen(4), hour.ToStringLen(2), minute.ToStringLen(2), second.ToStringLen(2));
+    }
+
+    static string GetTime(bool withSeconds = false)
+    {
+        string dateTimeFormat = "";
+        int hour = 0;
+        int minute = 0;
+        int second = 0;
+        GetHourMinuteSecond(hour, minute, second);
+
+        if (withSeconds)
+        {
+            dateTimeFormat = "%1:%2:%3";
+            return string.Format(dateTimeFormat, hour.ToStringLen(2), minute.ToStringLen(2), second.ToStringLen(2));
+        }
+        else
+        {
+            dateTimeFormat = "%1:%2";
+            return string.Format(dateTimeFormat, hour.ToStringLen(2), minute.ToStringLen(2));
+        }
+    }
 }
