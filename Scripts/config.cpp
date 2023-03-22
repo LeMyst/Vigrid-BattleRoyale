db0c9f9c012ec16ffe93a39403f4083b7ea10131
Myst <1592048+LeMyst@users.noreply.github.com>
2023-03-22 23:45:04 +0100

    Fix remove fog

--- Scripts/config.cpp
+++ Scripts/config.cpp
@@ -91,16 +91,19 @@ class CfgMods
 
 class CfgWorlds
 {
-	class DefaultWorld
-	{
-		class Weather
-		{
-			class VolFog
-			{
-				CameraFog=0;
-				Item1[]={0,0,0,0,0};
-				Item2[]={0,0,0,0,0};
-			};
-		};
-	};
+    class DefaultWorld;
+    class CAWorld: DefaultWorld
+    {
+        class Weather
+        {
+            class VolFog
+            {
+                CameraFog=0;
+                Item1[]={0,0,0,0,0};
+                Item2[]={0,0,0,0,0};
+            };
+        };
+    };
 };
+	{
+	};
