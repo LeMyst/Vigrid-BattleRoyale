b81019b9102e3947abbbf193ee6da85ab04b0491
Myst <1592048+LeMyst@users.noreply.github.com>
2024-03-08 13:15:14 +0100

    Add untranslated message to Play button

--- Scripts/5_Mission/Mission/MainMenu.c
+++ Scripts/5_Mission/Mission/MainMenu.c
@@ -3,7 +3,8 @@ modded class MainMenu
 {
     override void Play()
     {
-
+        // TODO: Translate this
+        GetGame().GetUIManager().ShowDialog("CLOSE YOUR GAME", "Please close your game and connect using your launcher.", 0, DBT_OK, DBB_NONE, DMT_INFO, this);
     }
 
     override void OpenMenuServerBrowser()
