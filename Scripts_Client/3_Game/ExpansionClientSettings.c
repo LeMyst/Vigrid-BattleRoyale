9d2a2137e370756ee5dde90cb446f2742c732a0e
LieutenantMaster <46745541+LieutenantMaster@users.noreply.github.com>
2024-03-08 01:45:36 +0100

    Create ExpansionClientSettings.c

---
+++ Scripts_Client/3_Game/ExpansionClientSettings.c
@@ -0,0 +1,52 @@
+modded class ExpansionClientSettings
+{
+	void SettingsOverride()
+	{
+        UseHelicopterMouseControl = true;
+        TurnOffAutoHoverDuringFlight = true;
+        EnableGPSBasedOnVehicleSpeed = true;
+
+		Show3DClientMarkers = true;
+		Show3DPlayerMarkers = true;
+		Show3DPartyMarkers = true;
+		Show3DGlobalMarkers = true;
+		Show3DPartyMemberIcon = true;
+		Show2DClientMarkers = true;
+		Show2DPlayerMarkers = true;
+		Show2DPartyMarkers = true;
+		Show2DGlobalMarkers = true;
+
+		MarkerSize = ExpansionClientUIMarkerSize.SMALL;
+		PlayerArrowColor = ExpansionClientUIPlayerArrowColor.YELLOW;
+	}
+
+	override void OnSettingsUpdated(typename type, ExpansionSettingSerializationBase setting)
+	{
+		SettingsOverride();
+
+		super.OnSettingsUpdated(type, setting);
+	}
+
+	override private void OnSave( ParamsWriteContext ctx )
+	{
+		SettingsOverride();
+
+		super.OnSave(ctx);
+    }
+
+	private bool OnRead( ParamsReadContext ctx, int version, out bool settingsRepaired = false )
+	{
+		if ( !super.OnRead(ctx, version, settingsRepaired) )
+			return false;
+
+		SettingsOverride();
+		return true;
+    }
+
+	override void Init()
+	{
+		super.Init();
+		
+		SettingsOverride();
+    }
+};
