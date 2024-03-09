8cdb4479b14fe3f46d4743021cbfeb18b85c7dae
LieutenantMaster <46745541+LieutenantMaster@users.noreply.github.com>
2024-03-09 16:18:35 +0100

    MainMenu changes

--- Scripts_Client/5_Mission/Mission/MainMenu.c
+++ Scripts_Client/5_Mission/Mission/MainMenu.c
@@ -1,10 +1,102 @@
 #ifndef SERVER
 modded class MainMenu
 {
+	override Widget Init()
+	{
+		layoutRoot = GetGame().GetWorkspace().CreateWidgets("Vigrid-BattleRoyale/GUI/layouts/MainMenu/main_menu.layout");
+		
+		m_Play						= layoutRoot.FindAnyWidget("play");
+		m_ChooseServer				= layoutRoot.FindAnyWidget("choose_server");
+		m_CustomizeCharacter		= layoutRoot.FindAnyWidget("customize_character");
+		m_PlayVideo					= layoutRoot.FindAnyWidget("play_video");
+		m_Tutorials					= layoutRoot.FindAnyWidget("tutorials");
+		m_TutorialButton			= layoutRoot.FindAnyWidget("tutorial_button");
+		m_MessageButton				= layoutRoot.FindAnyWidget("message_button");
+		m_SettingsButton			= layoutRoot.FindAnyWidget("settings_button");
+		m_Exit						= layoutRoot.FindAnyWidget("exit_button");
+		m_PrevCharacter				= layoutRoot.FindAnyWidget("prev_character");
+		m_NextCharacter				= layoutRoot.FindAnyWidget("next_character");
+
+		m_DlcFrame 					= layoutRoot.FindAnyWidget("dlc_Frame");
+		m_Version					= TextWidget.Cast(layoutRoot.FindAnyWidget("version"));
+		m_ModdedWarning				= TextWidget.Cast(layoutRoot.FindAnyWidget("ModdedWarning"));
+		m_CharacterRotationFrame	= layoutRoot.FindAnyWidget("character_rotation_frame");
+		
+		m_LastPlayedTooltip			= layoutRoot.FindAnyWidget("last_server_info");
+		m_LastPlayedTooltip.Show(false);
+		m_LastPlayedTooltipLabel	= m_LastPlayedTooltip.FindAnyWidget("last_server_info_label");
+		m_LastPlayedTooltipName 	= TextWidget.Cast(m_LastPlayedTooltip.FindAnyWidget("last_server_info_name"));
+		m_LastPlayedTooltipIP		= TextWidget.Cast(m_LastPlayedTooltip.FindAnyWidget("last_server_info_ip"));
+		m_LastPlayedTooltipPort		= TextWidget.Cast(m_LastPlayedTooltip.FindAnyWidget("last_server_info_port"));
+		
+		m_LastPlayedTooltipTimer	= new WidgetFadeTimer();
+		
+		m_Stats						= new MainMenuStats(layoutRoot.FindAnyWidget("character_stats_root"));
+		
+		m_Mission					= MissionMainMenu.Cast(GetGame().GetMission());
+		
+		m_LastFocusedButton 		= m_Play;
+
+		m_ScenePC					= m_Mission.GetIntroScenePC();
+		
+		if (m_ScenePC)
+		{
+			m_ScenePC.ResetIntroCamera();
+		}
+		
+		m_PlayVideo.Show(false);
+		
+		m_PlayerName				= TextWidget.Cast(layoutRoot.FindAnyWidget("character_name_text"));
+		
+		// Set Version
+		string version;
+		GetGame().GetVersion(version);
+		m_Version.SetText("#main_menu_version" + " " + version);
+		
+		GetGame().GetUIManager().ScreenFadeOut(0);
+
+		SetFocus(null);
+		
+		Refresh();
+		
+		LoadMods();
+		PopulateDlcFrame();
+		
+		GetDayZGame().GetBacklit().MainMenu_OnShow();
+		GetGame().GetMission().GetOnModMenuVisibilityChanged().Insert(ShowDlcFrame);
+	
+		g_Game.SetLoadState(DayZLoadState.MAIN_MENU_CONTROLLER_SELECT);
+
+		
+		BR_OverrideUI();
+		
+		return layoutRoot;
+	}
+
+	void BR_OverrideUI()
+	{
+		m_Play.Show(false);
+		m_ChooseServer.Show(false);
+		m_TutorialButton.Show(false);
+		m_DlcFrame.Show(false);
+
+		if (m_DisplayedDlcHandler)
+			m_DisplayedDlcHandler.ShowInfoPanel(false);
+	}
+
+	override void FilterDlcs(inout array<ref ModInfo> modArray)
+	{
+		BR_OverrideUI();
+	}
+
+	override void ShowDlcFrame(bool show)
+	{
+		super.ShowDlcFrame(false);
+	}
+
 	override void Play()
 	{
-		// TODO: Translate this
-		GetGame().GetUIManager().ShowDialog("CLOSE YOUR GAME", "Please close your game and connect using your launcher.", 0, DBT_OK, DBB_NONE, DMT_INFO, this);
+
 	}
 	
 	override void OpenMenuServerBrowser()
