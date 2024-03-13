ed797ac4687239c821a7c7c08bfab44bd6f6d7ac
Myst <1592048+LeMyst@users.noreply.github.com>
2024-03-13 23:24:12 +0100

    Fix Y axis when teleported back to spawn

--- Scripts_Server/5_Mission/BattleRoyale/Server/States/BattleRoyaleState.c
+++ Scripts_Server/5_Mission/BattleRoyale/Server/States/BattleRoyaleState.c
@@ -379,8 +379,8 @@ class BattleRoyaleDebugState: BattleRoyaleState
 
         vector spawn_pos = "0 0 0";
         spawn_pos[0] = Math.RandomFloatInclusive((v_Center[0] - 10), (v_Center[0] + 10));
-        spawn_pos[1] = GetGame().SurfaceY(v_Center[0], v_Center[2]);
         spawn_pos[2] = Math.RandomFloatInclusive((v_Center[2] - 10), (v_Center[2] + 10));
+        spawn_pos[1] = GetGame().SurfaceY(spawn_pos[0], spawn_pos[2]);
 
         vector playerPos = player.GetPosition();
         float distance = vector.Distance(playerPos, spawn_pos);
