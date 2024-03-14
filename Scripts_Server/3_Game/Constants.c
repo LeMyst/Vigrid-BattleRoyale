23028670b8666835a10eb0d65f52f7fb938982c8
Myst <1592048+LeMyst@users.noreply.github.com>
2024-03-15 00:27:46 +0100

    Change some stamina constants

---
+++ Scripts_Server/3_Game/Constants.c
@@ -0,0 +1,6 @@
+modded class GameConstants
+{
+	const int STAMINA_DRAIN_STANDING_SPRINT_PER_SEC = 0; //in units (how much sprint depletes stamina)
+	const int STAMINA_DRAIN_CROUCHED_SPRINT_PER_SEC = 0; //in units (how much sprint in crouch depletes stamina)
+	const float STAMINA_WEIGHT_LIMIT_THRESHOLD = 6000000; //! in grams (weight where the player is not penalized by stamina)
+}
