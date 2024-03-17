e404d9b2001ee43fa63db6b8b64a0121b54b1379
Myst <1592048+LeMyst@users.noreply.github.com>
2024-03-17 22:03:02 +0100

    Disable stamina while swiming

--- Scripts_Server/3_Game/Constants.c
+++ Scripts_Server/3_Game/Constants.c
@@ -2,5 +2,7 @@ modded class GameConstants
 {
 	const int STAMINA_DRAIN_STANDING_SPRINT_PER_SEC = 0; //in units (how much sprint depletes stamina)
 	const int STAMINA_DRAIN_CROUCHED_SPRINT_PER_SEC = 0; //in units (how much sprint in crouch depletes stamina)
+	const int STAMINA_DRAIN_SWIM_FAST_PER_SEC = 0; //in units (how much fast swimming depletes stamina)
+
 	const float STAMINA_WEIGHT_LIMIT_THRESHOLD = 6000000; //! in grams (weight where the player is not penalized by stamina)
 }
