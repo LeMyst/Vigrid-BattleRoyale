5e15eeec80170a35b54a0a48d6014fdc369cf161
Myst <1592048+LeMyst@users.noreply.github.com>
2024-03-20 09:04:19 +0100

    Prepare for 0.0.14

--- mod.cpp
+++ mod.cpp
@@ -6,4 +6,4 @@ logoOver = "Vigrid-BattleRoyale/GUI/textures/Mod_Logo.paa";      // on mouse hov
 tooltip = "DayZ Battle Royale";                         // tooltip on mouse hover
 overview = "DayZ Battle Royale Mod";                    // overview
 author = "From mod.cpp";                                // author
-version = "0.0.13-Vigrid";                                 // version
+version = "0.0.14-Vigrid";                                 // version
