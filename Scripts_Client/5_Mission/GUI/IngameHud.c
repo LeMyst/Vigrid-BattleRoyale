0d4e1e53c6f432e9476fd9f40935772361ed07f1
Myst <1592048+LeMyst@users.noreply.github.com>
2024-03-27 12:39:35 +0100

    Change color of name in Lobby

---
+++ Scripts_Client/5_Mission/GUI/IngameHud.c
@@ -0,0 +1,24 @@
+#ifndef SERVER
+modded class IngameHud
+{
+	override protected bool HandleCurrentTaggedPlayer(float timeslice)
+	{
+		bool result = super.HandleCurrentTaggedPlayer(timeslice);
+
+		DayZPlayerImplement localPlayer = DayZPlayerImplement.Cast(GetGame().GetPlayer());
+		if ( localPlayer.Expansion_IsInSafeZone() )
+		{
+			if (m_IsFriendly || m_IsMember)
+			{
+				m_PlayerTagText.SetColor(ARGB( 255, 0, 190, 255 ));
+			}
+			else
+			{
+				m_PlayerTagText.SetColor(COLOR_EXPANSION_ITEM_NORMAL_TEXT);
+			}
+		}
+
+		return result;
+	}
+}
+#endif
