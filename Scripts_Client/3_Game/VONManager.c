7aa30e541534fb2c07b1e3eb15a61ed9bf4e36e5
Myst <1592048+LeMyst@users.noreply.github.com>
2024-03-28 15:08:14 +0100

    Improve VoN options

--- Scripts_Client/3_Game/VONManager.c
+++ Scripts_Client/3_Game/VONManager.c
@@ -1,14 +1,72 @@
 #ifndef SERVER
+modded class VONManagerBase
+{
+	void SetMaxVolume(int max_level);
+	void EnableVoice(bool enabled);
+}
+
 modded class VONManagerImplementation
 {
+	protected int max_voice_level = VoiceLevelWhisper;
+	protected int voice_enabled = false;
+
 	override void HandleInput(Input inp)
 	{
-		super.HandleInput( inp );
+		int oldLevel = GetGame().GetVoiceLevel();
+		if (oldLevel == -1) //VoN system not initialized!
+			return;
 
-		if ( GetGame().GetVoiceLevel() > VoiceLevelWhisper )
+		int newLevel = -1;
+
+		if (inp.LocalPress_ID(UAVoiceDistanceUp,false))
 		{
-			GetGame().SetVoiceLevel( VoiceLevelWhisper ); // Force level to whisper
+			newLevel = ( oldLevel + 1 ) % ( max_voice_level + 1 );
+		}
+
+		if (inp.LocalPress_ID(UAVoiceDistanceDown,false))
+		{
+			newLevel = oldLevel - 1;
+			if (newLevel < VoiceLevelWhisper) //nah...
+			{
+				newLevel = max_voice_level;
+			}
+		}
+
+		if (newLevel > -1)
+		{
+			GetGame().SetVoiceLevel(newLevel);
 			UpdateVoiceIcon();
 		}
+	}
+
+	override void OnEvent(EventType eventTypeId, Param params)
+	{
+		switch (eventTypeId)
+		{
+			case VONStateEventTypeID:
+			{
+				if ( !voice_enabled )
+				{
+					break;
+				}
+
+				super.OnEvent( eventTypeId, params )
+				break;
+			}
+			default:
+			{
+				super.OnEvent( eventTypeId, params );
+			}
+		}
+	}
+
+	void SetMaxVolume(int max_level)
+	{
+		max_voice_level = max_level;
+	}
+
+	void EnableVoice(bool enabled)
+	{
+		voice_enabled = enabled;
 	}
 }
