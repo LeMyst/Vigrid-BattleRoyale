2584b3e1335b958ff8e39b521dc90e5a1bea0610
Myst <1592048+LeMyst@users.noreply.github.com>
2024-04-01 00:25:37 +0200

    Random fuel amount

--- Scripts_Server/4_World/Entities/Vehicles/CarScript.c
+++ Scripts_Server/4_World/Entities/Vehicles/CarScript.c
@@ -4,7 +4,7 @@ modded class CarScript
     override void EEOnCECreate()
     {
         // Fill the car to max
-        Fill( CarFluid.FUEL, GetFluidCapacity( CarFluid.FUEL ) );
+        Fill( CarFluid.FUEL, GetFluidCapacity( CarFluid.FUEL ) * Math.RandomFloatInclusive(0.30, 1) );
         Fill( CarFluid.COOLANT, GetFluidCapacity( CarFluid.COOLANT ) );
         Fill( CarFluid.OIL, GetFluidCapacity( CarFluid.OIL ) );
     }
