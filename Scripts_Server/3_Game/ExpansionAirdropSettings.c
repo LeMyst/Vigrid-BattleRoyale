afa76e8fc95aaf72158056e951b27df5088eff29
Myst <1592048+LeMyst@users.noreply.github.com>
2024-03-31 21:52:34 +0200

    Disable Airdrop settings override

--- Scripts_Server/3_Game/ExpansionAirdropSettings.c
+++ Scripts_Server/3_Game/ExpansionAirdropSettings.c
@@ -1,28 +1,26 @@
-#ifdef SERVER
-modded class ExpansionAirdropSettings
-{
-	void SettingsOverride()
-	{
-        Height = 250.0;
-        Radius = 1.0;
-        Speed = 400;
-	}
-
-	override void OnSave()
-	{
-		SettingsOverride();
-
-		super.OnSave();
-    }
-
-	override bool OnRead()
-	{
-		if ( !super.OnRead() )
-			return false;
-
-		SettingsOverride();
-
-		return true;
-    }
-}
-#endif
+//modded class ExpansionAirdropSettings
+//{
+//	void SettingsOverride()
+//	{
+//        Height = 250.0;
+//        Radius = 1.0;
+//        Speed = 400;
+//	}
+//
+//	override bool OnSave()
+//	{
+//		SettingsOverride();
+//
+//		super.OnSave();
+//    }
+//
+//	override bool OnLoad()
+//	{
+//		if ( !super.OnLoad() )
+//			return false;
+//
+//		SettingsOverride();
+//
+//		return true;
+//    }
+//}
