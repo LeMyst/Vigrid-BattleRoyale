cb28a811151fad3119f8c7d6c82297d2fa6377ee
Myst <1592048+LeMyst@users.noreply.github.com>
2024-04-13 12:14:21 +0200

    Improve IngameHud.c

--- Scripts_Client/5_Mission/GUI/IngameHud.c
+++ Scripts_Client/5_Mission/GUI/IngameHud.c
@@ -1,7 +1,29 @@
 #ifndef SERVER
-#ifdef EXPANSIONMODNAMETAGS
 modded class IngameHud
 {
+    protected bool b_BrVisibilityOverride;
+
+    void IngameHud()
+    {
+        b_BrVisibilityOverride = false;
+    }
+
+    void BR_HIDE()
+    {
+        Show( false );
+
+        b_BrVisibilityOverride = true;
+    }
+
+    override void Show( bool show )
+    {
+        if(b_BrVisibilityOverride)
+            return;
+
+        super.Show( show );
+    }
+
+#ifdef EXPANSIONMODNAMETAGS
 	override protected bool HandleCurrentTaggedPlayer(float timeslice)
 	{
 		bool result = super.HandleCurrentTaggedPlayer(timeslice);
@@ -21,6 +43,6 @@ modded class IngameHud
 
 		return result;
 	}
-}
 #endif
+}
 #endif
