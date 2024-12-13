22f84609e8455828d42e80765ac7e94fdbfd7e29
Myst <1592048+LeMyst@users.noreply.github.com>
2024-12-13 21:55:11 +0100

    Move hints

--- Scripts_Client/5_Mission/GUI/Hints/UiHintPanel.c
+++ Scripts_Client/5_Mission/GUI/Hints/UiHintPanel.c
@@ -3,6 +3,6 @@
     //load BR hints instead of native engines
     private override void LoadContentList()
     {
-        JsonFileLoader<array<ref HintPage>>.JsonLoadFile( "Vigrid-BattleRoyale/Scripts/5_Mission/GUI/Hints/hints.json", m_ContentList );
+        JsonFileLoader<array<ref HintPage>>.JsonLoadFile( "Vigrid-BattleRoyale/Data/hints.json", m_ContentList );
     }
 }*/
