446d0752ad98916c56caddf550c4886335e078e8
Myst <1592048+LeMyst@users.noreply.github.com>
2025-03-12 04:20:33 +0100

    Delete BrokenLegs.c

--- Scripts_Server/4_World/Classes/PlayerModifiers/Modifiers/Conditions/BrokenLegs.c
+++
@@ -1,8 +0,0 @@
-#ifdef SERVER
-modded class BrokenLegsMdfr
-{
-    override void OnActivate(PlayerBase player) {}
-    override void OnReconnect(PlayerBase player) {}
-    override void Activate() {}
-};
-#endif
