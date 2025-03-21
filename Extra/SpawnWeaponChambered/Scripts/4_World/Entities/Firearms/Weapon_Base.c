4716ada95b26b0881d09b177648712a9c2ef5fa2
Myst <1592048+LeMyst@users.noreply.github.com>
2025-03-21 22:11:51 +0100

    Fix prevent spawning magazine if already one exists

--- Extra/SpawnWeaponChambered/Scripts/4_World/Entities/Firearms/Weapon_Base.c
+++ Extra/SpawnWeaponChambered/Scripts/4_World/Entities/Firearms/Weapon_Base.c
@@ -7,5 +7,11 @@ modded class Weapon_Base
 
 		// Chamber the weapon with a random bullet by default
 		FillChamber( "", WeaponWithAmmoFlags.CHAMBER );
+
+		Magazine mag = GetMagazine(0);
+		if ( mag == NULL )  // Can't spawn a magazine if there's already one
+		{
+			SpawnAttachedMagazine( "", WeaponWithAmmoFlags.CHAMBER | WeaponWithAmmoFlags.QUANTITY_RNG );
+		}
 	}
 }
