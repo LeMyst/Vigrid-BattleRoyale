b65b822d8d46a61568e42d4e3e4e37c245094501
Myst <1592048+LeMyst@users.noreply.github.com>
2025-05-11 22:43:17 +0200

    Random Battery Health (#137)

--- Extra/SpawnWithBattery/Scripts/4_World/Entities/ItemBase.c
+++ Extra/SpawnWithBattery/Scripts/4_World/Entities/ItemBase.c
@@ -8,9 +8,24 @@ modded class ItemBase
 
 		if( HasEnergyManager() )
 		{
-			GetInventory().CreateInInventory( "Battery9V" );  // Try to spawn a 9V battery in the inventory
-			GetInventory().CreateInInventory( "CarBattery" );  // Try to spawn a car battery in the inventory
-			// TODO: Random health and quantity for better experience
+			ItemBase item;
+			item = ItemBase.Cast( GetInventory().CreateInInventory( "Battery9V" ) );  // Try to spawn a 9V battery in the inventory
+			ApplyRandomHealthAndEnergy(item);  // Apply random health and quantity
+			item = ItemBase.Cast( GetInventory().CreateInInventory( "CarBattery" ) );  // Try to spawn a car battery in the inventory
+			ApplyRandomHealthAndEnergy(item);  // Apply random health and quantity
+		}
+	}
+
+	static const float HEALTH_MIN_FACTOR = 0.66;  // Minimum health as a fraction of max health
+	static const float ENERGY_MIN = 0.5;         // Minimum energy level
+	static const float ENERGY_MAX = 0.9;         // Maximum energy level
+
+	void ApplyRandomHealthAndEnergy(ItemBase item)
+	{
+		if (item)
+		{
+			item.SetHealth(Math.RandomIntInclusive(item.GetMaxHealth() * HEALTH_MIN_FACTOR, item.GetMaxHealth()));  // Random health
+			item.GetCompEM().SetEnergy0To1(Math.RandomFloatInclusive(ENERGY_MIN, ENERGY_MAX));  // Random energy level
 		}
 	}
 }
