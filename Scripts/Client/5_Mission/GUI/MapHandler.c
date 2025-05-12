62e8c934f3f1369df56180771b66e16f495b35c0
Myst <1592048+LeMyst@users.noreply.github.com>
2025-05-12 23:33:03 +0200

    Refactor map click handling to improve spawn selection responsiveness

--- Scripts/Client/5_Mission/GUI/MapHandler.c
+++ Scripts/Client/5_Mission/GUI/MapHandler.c
@@ -3,42 +3,37 @@ class BRMapHandler : ScriptedWidgetEventHandler
 {
 	protected Widget m_Root;
 
+	protected int last_click_on_map;
+	protected vector v_StartClickPos;
+
 	void BRMapHandler(Widget w)
 	{
 		m_Root = w;
 		m_Root.SetHandler(this);
+		last_click_on_map = 0;
 	}
 
-	override bool OnKeyDown(Widget w, int x, int y, int key)
+	// When the player clicks on the map
+	override bool OnMouseButtonDown(Widget w, int x, int y, int button)
 	{
-		if (!super.OnKeyDown(w, x, y, key))
-			return false;
+		v_StartClickPos = Vector(x, y, 0);
 
-		GetGame().Chat("BRMapHandler::OnKeyDown", "colorFriendly");
-		return true;
+		return super.OnMouseButtonDown(w, x, y, button);
 	}
 
-	override bool OnClick(Widget w, int x, int y, int button)
+	override bool OnMouseButtonUp(Widget w, int x, int y, int button)
 	{
-		super.OnClick(w, x, y, button);
+		if(GetGame().GetTime() - last_click_on_map < 250 || v_StartClickPos != Vector(x, y, 0))
+		{
+			return super.OnMouseButtonUp(w, x, y, button);
+		}
+		last_click_on_map = GetGame().GetTime();
 
-		GetGame().Chat("BRMapHandler::OnClick", "colorFriendly");
+		GetGame().Chat("BRMapHandler::OnMouseButtonDown", "colorFriendly");
 
 		SpawnSelectionMenu m = SpawnSelectionMenu.Cast(g_Game.GetUIManager().FindMenu(MENU_SPAWN_SELECTION));
 		m.SelectSpawnPoint(Vector(x, y, 0));
 
-		return true;
-	}
-
-	override bool OnDoubleClick(Widget w, int x, int y, int button)
-	{
-		super.OnDoubleClick(w, x, y, button);
-
-		GetGame().Chat("BRMapHandler::OnDoubleClick", "colorFriendly");
-
-		SpawnSelectionMenu m = SpawnSelectionMenu.Cast(g_Game.GetUIManager().FindMenu(MENU_SPAWN_SELECTION));
-		m.SelectSpawnPoint(Vector(x, y, 0));
-
-		return true;
+		return super.OnMouseButtonUp(w, x, y, button);
 	}
 };
